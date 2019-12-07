a90ed1a29558f0b23ea4e3973d8e5812edb5fb03
AnatoliyVinokurov <anatolij.isit@gmail.com>
2019-12-07 23:14:04 +0300

    №5 Added Menu Android Studio

--- app/src/main/java/com/anatoliyvinokurov/quizgamekotlin/MainActivity.kt
+++ app/src/main/java/com/anatoliyvinokurov/quizgamekotlin/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.anatoliyvinokurov.quizgamekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonStart = findViewById<Button>(R.id.button_start)
+        buttonStart.setOnClickListener {
+            try {
+                val intent = Intent(this@MainActivity, GameLevels::class.java)
+                startActivity(intent)
+                finish()
+            } catch (e:Exception) {
+            }
+        }
 
         val w = getWindow()
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
