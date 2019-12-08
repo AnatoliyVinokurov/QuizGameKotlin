e9ce7c18c51274b2d00c43147d5155496449659a
AnatoliyVinokurov <anatolij.isit@gmail.com>
2019-12-08 19:07:10 +0300

    №7 code for on-screen and system Back button, onbackpressed Android.

--- app/src/main/java/com/anatoliyvinokurov/quizgamekotlin/GameLevels.kt
+++ app/src/main/java/com/anatoliyvinokurov/quizgamekotlin/GameLevels.kt
@@ -1,8 +1,10 @@
 package com.anatoliyvinokurov.quizgamekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 
 class GameLevels : AppCompatActivity() {
 
@@ -10,7 +12,30 @@ class GameLevels : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gamelevels)
 
+        //code to deploy the application to full screen
         val w = getWindow()
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        // code for on-screen Back button"
+        val buttonStart = findViewById<Button>(R.id.button_back)
+        buttonStart.setOnClickListener {
+            try {
+                val intent = Intent(this@GameLevels, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } catch (e:Exception) {
+            }
+        }
+
+    }
+
+    // code for system back button
+    override fun onBackPressed() {
+        try {
+            val intent = Intent(this@GameLevels, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        } catch (e:Exception) {
+        }
     }
 }
