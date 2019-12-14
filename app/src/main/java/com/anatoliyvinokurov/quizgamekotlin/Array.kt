2d775328e315257927caa11c4c4c060996ff9be9
AnatoliyVinokurov <anatolij.isit@gmail.com>
2019-12-14 22:55:06 +0300

    #25-26: Added fifteen images with animals. Created an Array class containing three arrays with animal names, weight number, path to images

---
+++ app/src/main/java/com/anatoliyvinokurov/quizgamekotlin/Array.kt
@@ -0,0 +1,41 @@
+package com.anatoliyvinokurov.quizgamekotlin
+
+class Array {
+
+    //Array for the first level
+    val images1 = intArrayOf(
+        R.drawable.onelevel_zero,
+        R.drawable.onelevel_two,
+        R.drawable.onelevel_three,
+        R.drawable.onelevel_four,
+        R.drawable.onelevel_fife,
+        R.drawable.onelevel_six,
+        R.drawable.onelevel_seven,
+        R.drawable.onelevel_eight,
+        R.drawable.onelevel_nine,
+        R.drawable.onelevel_ten,
+        R.drawable.onelevel_eleven,
+        R.drawable.onelevel_twelve,
+        R.drawable.onelevel_thirteen,
+        R.drawable.onelevel_forteen)
+
+
+    val power1 = intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14)
+
+    val texts1 = intArrayOf(
+        R.string.lvl1text0,
+        R.string.lvl1text1,
+        R.string.lvl1text2,
+        R.string.lvl1text3,
+        R.string.lvl1text4,
+        R.string.lvl1text5,
+        R.string.lvl1text6,
+        R.string.lvl1text7,
+        R.string.lvl1text8,
+        R.string.lvl1text9,
+        R.string.lvl1text10,
+        R.string.lvl1text11,
+        R.string.lvl1text12,
+        R.string.lvl1text13,
+        R.string.lvl1text14)
+}
